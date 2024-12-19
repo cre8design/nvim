8a5eb05588bfc5d645a75ee672d87a6e89e97b97
Yannik <yannik@printingambitions.com>
2024-12-19 13:51:22 +0100

    Changed keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local map = LazyVim.safe_keymap_set
-map("n", "<CR>", "m`o<Esc>``")
-map("n", "<S-CR>", "m`O<Esc>``")
+local map = vim.keymap.set
+
+map("n", "<C-n>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-e>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-i>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-o>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+map("n", "<S-n>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<S-o>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
