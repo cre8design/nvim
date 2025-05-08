c17d979eef65f78ae6d4f433a7010c358bf92775
Yannik <yannik@printingambitions.com>
2025-05-08 16:26:52 +0200

    dev

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -102,3 +102,113 @@ end)
 vim.keymap.set("n", "<leader>uw", function()
 	vim.o.wrap = not vim.o.wrap
 end)
+
+-- better up/down
+vim.keymap.set(
+	{ "n", "x" },
+	"j",
+	"v:count == 0 ? 'gj' : 'j'",
+	{ desc = "Down", expr = true, silent = true }
+)
+vim.keymap.set(
+	{ "n", "x" },
+	"<Down>",
+	"v:count == 0 ? 'gj' : 'j'",
+	{ desc = "Down", expr = true, silent = true }
+)
+vim.keymap.set(
+	{ "n", "x" },
+	"k",
+	"v:count == 0 ? 'gk' : 'k'",
+	{ desc = "Up", expr = true, silent = true }
+)
+vim.keymap.set(
+	{ "n", "x" },
+	"<Up>",
+	"v:count == 0 ? 'gk' : 'k'",
+	{ desc = "Up", expr = true, silent = true }
+)
+
+-- Clear search and stop snippet on escape
+vim.keymap.set({ "i", "n", "s" }, "<esc>", function()
+	vim.cmd("noh")
+	return "<esc>"
+end, { expr = true, desc = "Escape and Clear hlsearch" })
+
+-- Clear search, diff update and redraw
+-- taken from runtime/lua/_editor.lua
+vim.keymap.set(
+	"n",
+	"<leader>ur",
+	"<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
+	{ desc = "Redraw / Clear hlsearch / Diff Update" }
+)
+
+-- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+vim.keymap.set(
+	"n",
+	"n",
+	"'Nn'[v:searchforward].'zv'",
+	{ expr = true, desc = "Next Search Result" }
+)
+vim.keymap.set(
+	"x",
+	"n",
+	"'Nn'[v:searchforward]",
+	{ expr = true, desc = "Next Search Result" }
+)
+vim.keymap.set(
+	"o",
+	"n",
+	"'Nn'[v:searchforward]",
+	{ expr = true, desc = "Next Search Result" }
+)
+vim.keymap.set(
+	"n",
+	"N",
+	"'nN'[v:searchforward].'zv'",
+	{ expr = true, desc = "Prev Search Result" }
+)
+vim.keymap.set(
+	"x",
+	"N",
+	"'nN'[v:searchforward]",
+	{ expr = true, desc = "Prev Search Result" }
+)
+vim.keymap.set(
+	"o",
+	"N",
+	"'nN'[v:searchforward]",
+	{ expr = true, desc = "Prev Search Result" }
+)
+
+-- Add undo break-points
+vim.keymap.set("i", ",", ",<c-g>u")
+vim.keymap.set("i", ".", ".<c-g>u")
+vim.keymap.set("i", ";", ";<c-g>u")
+
+-- save file
+vim.keymap.set(
+	{ "i", "x", "n", "s" },
+	"<C-s>",
+	"<cmd>w<cr><esc>",
+	{ desc = "Save File" }
+)
+
+-- better indenting
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- commenting
+vim.keymap.set(
+	"n",
+	"gco",
+	"o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>",
+	{ desc = "Add Comment Below" }
+)
+vim.keymap.set(
+	"n",
+	"gcO",
+	"O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>",
+	{ desc = "Add Comment Above" }
+)
